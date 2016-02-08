c444c5f38c785bda41c9690e0bcf5709b403f40d
u09068ee <u09068ee@gmail.com>
2016-02-08 23:16:50 +0000

    db user/pass update

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $db = new PDO('mysql:host=localhost;dbname=wordo;charset=utf8', 'root', 'woiwala@EB', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $db = new PDO('mysql:host=localhost;dbname=wordo;charset=utf8', 'root', 'gIZCtmp4F0', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     } catch (Exception $e) {
         echo 'Can\'t Connect to Database at this time: ',  $e->getMessage(), "\n";
         header('Location: ../');
